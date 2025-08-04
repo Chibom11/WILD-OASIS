d2c7210d460a5b2ab7d0a62067b8ec84a5ff3e9d
Chibom11 <shivamsurroach10@gmail.com>
2025-08-05 01:46:44 +0530

    fixed export

--- components/CurrentTrips.jsx
+++ components/CurrentTrips.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useState,useEffect}  from 'react'
 import axios from 'axios'
-
+import toast from 'react-hot-toast';
 import { useOutletContext } from 'react-router-dom';
 
 
